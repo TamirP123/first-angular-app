a58074e00cbfc9c69e0376c392b3588e13d75f13
Tamir <tamirphillips@live.com>
2024-11-08 20:10:34 -0500

    Passing data from parent to child component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [GreetingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+homeMessage = signal('Hello World!');
 }
