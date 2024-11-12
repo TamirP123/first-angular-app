7943d0701ca11ab708a00e379db49970f325d481
Tamir <tamirphillips@live.com>
2024-11-11 19:07:32 -0500

    Angular Directives

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
+import { NgIf } from '@angular/common';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
@@ -25,4 +27,19 @@ ngOnInit(): void {
     this.todoItems.set(todos);
   })
 }
+
+updateTodoItem(todoItem: Todo) {
+  this.todoItems.update((todos) => {
+    return todos.map(todo => {
+      if(todo.id === todoItem.id) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      }
+      return todo;
+    });
+  });
+}
+
 }
